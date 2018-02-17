6542b6a7026e831eba5f986499da2b7ca0fa365d
Aidan Wolk <aidanwolk@comcast.net>
2018-02-17 15:46:03 -0800

    Create basic terminal emulator (non-GUI)

---
+++ src/term_em.py
@@ -0,0 +1,21 @@
+import pyte
+
+
+class TerminalEmulator:
+    def __init__(self, connection):
+        self._connection = connection
+        self._screen = pyte.Screen(80, 24)
+        self._stream = pyte.ByteStream(self._screen)
+
+    def write(self, in_bytes):
+        """Write bytes from client to server"""
+        self._connection.send_ssh_bytes(in_bytes)
+
+    def receive(self):
+        """Receive bytes from server"""
+        if self._connection.has_ssh_bytes():
+            self._stream.feed(self._connection.receive_ssh_data())
+
+    def resize(self, cols=80, rows=24):
+        self._screen.resize(cols, rows)
+        self._connection.resize_term(cols, rows)
