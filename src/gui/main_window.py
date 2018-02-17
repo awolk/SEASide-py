5890a823b5e8acec647b42ea187b268ae25b95e5
Aidan Wolk <aidanwolk@comcast.net>
2018-02-17 13:14:12 -0800

    Add basic main window views

--- src/gui/main_window.py
+++ src/gui/main_window.py
@@ -1,6 +1,37 @@
-from kivy.uix.widget import Widget
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.spinner import Spinner
+from kivy.uix.button import Button
+
+SERVERS = {
+    'lnxsrv': 'lnxsrv.seas.ucla.edu',
+    'lnxsrv07': 'lnxsrv07.seas.ucla.edu',
+    'lnxsrv09': 'lnxsrv09.seas.ucla.edu'
+}
 
 
-class MainWindow(Widget):
+class MainWindow(FloatLayout):
     def __init__(self):
         super(MainWindow, self).__init__()
+        server_names = tuple(sorted(SERVERS.keys()))
+        spinner = Spinner(
+            text=server_names[0],
+            values=server_names,
+            size_hint=(None, None),
+            size=(300, 60)
+        )
+        button = Button(
+            text='Connect',
+            size=(300, 60),
+            size_hint=(None, None)
+        )
+        top_layout = BoxLayout(
+            spacing=10,
+            orientation='horizontal',
+            pos_hint={'top': 1, 'right': 1},
+            size_hint=(None, None),
+            size=(610, 60)
+        )
+        top_layout.add_widget(button)
+        top_layout.add_widget(spinner)
+        self.add_widget(top_layout)
