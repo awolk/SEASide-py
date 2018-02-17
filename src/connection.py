22889eb0fdb521d49f6ba582a4a45419c53df2d3
Ken Gu <ken.qgu@gmail.com>
2018-02-17 13:47:17 -0800

    "try merge with deven"

--- src/connection.py
+++ src/connection.py
@@ -11,22 +11,6 @@ class Connection:
     def attempt_login(username, password):
         pass
     def send_ssh_bytes(bytes):
-        # Check if connection is made previously
-        if (self.client):
-            stdin, stdout, stderr = self.client.exec_command(command)
-            while not stdout.channel.exit_status_ready():
-                # Print stdout data when available
-                if stdout.channel.recv_ready():
-                    # Retrieve the first 1024 bytes
-                    alldata = stdout.channel.recv(1024)
-                    while stdout.channel.recv_ready():
-                        # Retrieve the next 1024 bytes
-                        alldata += stdout.channel.recv(1024)
-
-                    # Print as string with utf8 encoding
-                    print(str(alldata, "utf8"))
-        else:
-            print("Connection not opened.")
         pass
     def has_ssh_data(self):
         """returns True if there is buffered data returns False otherwise"""
