3d2e20c45704ebc9f9b029690259a06ae3f83d08
Ken Gu <ken.qgu@gmail.com>
2018-02-17 20:20:07 -0800

    "bugged file explorer"

--- src/gui/fileexplorer.py
+++ src/gui/fileexplorer.py
@@ -1,34 +1,34 @@
 from kivy.uix.widget import Widget
-from kivy.uix.filechooser import FileChooserListLayout, FileChooserIconLayout, FileChooser, FileSystemAbstract
+from kivy.uix.filechooser import FileSystemLocal, FileChooserListLayout, FileChooser, FileSystemAbstract
 
 
-class FileExplorer(Widget):
+class FileExplorer(FileChooser):
     def start(self):
         self.connection = self.parent.get_connection()
         self.file_system = FileSystemRemote(self.connection)
-        self.add_widget(FileChooser(file_system=self.file_system))
-
+        #self.add_widget(FileChooser(file_system=self.file_system))
+        self.add_widget(FileChooserListLayout())
 
 class FileSystemRemote(FileSystemAbstract):
     def __init__(self, connection):
         super(FileSystemRemote, self).__init__()
         self.connection = connection
 
-    def get_size(self, fn):
+    def getsize(self, fn):
         print("size")
         return self.connection.get_size(fn)
 
     def is_dir(self, fn):
+        fn = "/" + fn[1:]
         print("isdir" + fn)
         print(self.connection.is_dir(fn))
         return self.connection.is_dir(fn)
 
     def is_hidden(self, fn):
-        print("hidden")
-        return False
+        return fn[1] == "."
 
     def listdir(self, fn):
+        #fn = fn[1:]
         print("listdir" + fn)
         print(self.connection.list_dir(fn))
         return self.connection.list_dir(fn)
-
