087cd2bd8d1a4118856f16cb68b693c224f4b296
Aidan Wolk <aidanwolk@comcast.net>
2018-02-17 19:20:31 -0800

    Update window settings

--- src/app.py
+++ src/app.py
@@ -1,6 +1,10 @@
 from kivy.app import App
+from kivy.config import Config
+
 from src.gui.main_window import MainWindow
 
+Config.set('kivy', 'exit_on_escape', 0)
+Config.set('graphics', 'resizable', 0)
 
 class SEASideApp(App):
     def build(self):
