8a6d66fb7a2267c03c83070cd278f0ddf5884a34
Aidan Wolk <aidanwolk@comcast.net>
2018-02-19 01:44:39 -0800

    Refactor Connection object

--- src/connection.py
+++ src/connection.py
@@ -5,41 +5,44 @@ class Connection:
     def __init__(self, server):
         self._server = server
         self._client = paramiko.SSHClient()
+        self._client.load_system_host_keys()
+        self._client.set_missing_host_key_policy(paramiko.client.AutoAddPolicy())
         self._chan = None
-    def create_chan(self):
+        self._sftp = None
+        self._home_dir = None
+
+    def _create_chan(self):
         try:
             self._chan = self._client.invoke_shell()
         except paramiko.ssh_exception.SSHException as e:
             print(e)
 
-    def create_sftp(self):
+    def _create_sftp(self):
         self._sftp = self._client.open_sftp()
+        self._sftp.chdir('.')
+        self._home_dir = self._sftp.getcwd()
 
     def attempt_connection(self, username):
-        """ returns True if connection successful returns False is unable to authenticate"""
-        self._client.load_system_host_keys()
-        self._client.set_missing_host_key_policy(paramiko.client.AutoAddPolicy())
+        """Returns True if connection successful returns False is unable to authenticate"""
         try:
             self._client.connect(self._server, username=username, look_for_keys=True)
         except (paramiko.AuthenticationException, paramiko.ssh_exception.SSHException) as e:
             print(e)
             return False
         else:
-            self.create_chan()
-            self.create_sftp()
+            self._create_chan()
+            self._create_sftp()
             return True
 
     def attempt_login(self, username, password):
-        self._client.load_system_host_keys()
-        self._client.set_missing_host_key_policy(paramiko.client.AutoAddPolicy())
         try:
             self._client.connect(self._server, username=username, password=password)
         except (paramiko.AuthenticationException, paramiko.ssh_exception.SSHException) as e:
             print(e)
             return False
         else:
-            self.create_chan()
-            self.create_sftp()
+            self._create_chan()
+            self._create_sftp()
             return True
 
     def send_ssh_bytes(self, bytes):
@@ -58,7 +61,7 @@ class Connection:
     def receive_ssh_data(self):
         """returns buffered received data"""
         buffer = b""
-        while self._chan != None and self.has_ssh_data():
+        while self._chan is not None and self.has_ssh_data():
             buffer += self._chan.recv(1024)
         return buffer
 
@@ -77,7 +80,6 @@ class Connection:
     def list_dir(self, path):
         prefix = path + '/' if path != '/' else path
         return [prefix + filename for filename in self._sftp.listdir(path)]
-
 
     def is_dir(self, path):
         try:
