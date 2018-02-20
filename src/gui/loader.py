37537b1ad0d3e684e5f90c8569f5ddcd252933ab
Aidan Wolk <aidanwolk@comcast.net>
2018-02-20 14:56:28 -0800

    Remove outdated code

--- src/gui/loader.py
+++ src/gui/loader.py
@@ -1,24 +1,3 @@
-# from kivy.uix.floatlayout import FloatLayout
-# from kivy.uix.label import Label
-#
-#
-# class Loader(FloatLayout):
-#     def __init__(self):
-#         super(Loader, self).__init__(size_hint=(1, 1))
-#         self.add_widget(Label(
-#             text='Loading...',
-#             pos_hint={'center_x': 0.5, 'center_y': 0.5},
-#         ))
-#
-#     def connect(self, username=None, password=None):
-#         if not username:
-#             return self.parent.connection_failed()
-#         conn = self.parent.get_connection()
-#         success = conn.attempt_login(username, password) if password else conn.attempt_connection(username)
-#         if success:
-#             self.parent.connection_successful()
-#         else:
-#             self.parent.connection_failed('Login failed')
 from PyQt5.QtWidgets import QLabel
 from PyQt5.QtCore import Qt
 
