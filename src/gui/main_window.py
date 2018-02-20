8cacd34b063c0ddcbd7ad1b6f8d833a72c5f8b0f
Aidan Wolk <aidanwolk@comcast.net>
2018-02-20 00:42:21 -0800

    Add tabs to main window

--- src/gui/main_window.py
+++ src/gui/main_window.py
@@ -58,7 +58,8 @@
 #     def get_username(self):
 #         return self._config.get_username()
 
-from PyQt5.QtWidgets import QMainWindow, QComboBox, QPushButton, QHBoxLayout, QWidget
+from PyQt5.QtWidgets import QMainWindow, QComboBox, QPushButton, QHBoxLayout, QVBoxLayout, QWidget, QTabWidget
+from PyQt5.QtCore import pyqtSlot
 
 #from gui.master_tab import MasterTab
 from server_config import SERVERS
@@ -74,13 +75,41 @@ class MainWindow(QMainWindow):
         self._config = Configuration()
         server_names = tuple(sorted(SERVERS.keys()))
         # Interface
-        wid = QWidget(self)
-        self.setCentralWidget(wid)
-        layout = QHBoxLayout(self)
-        dropdown = QComboBox()
-        dropdown.addItems(server_names)
-        dropdown.setCurrentIndex(dropdown.findText(self._config.get_default_server()))
-        button = QPushButton('Connect')
-        layout.addWidget(button)
-        layout.addWidget(dropdown)
-        wid.setLayout(layout)
+        self._widg = QWidget(self)
+        self.setCentralWidget(self._widg)
+        # Server selection dropdown
+        self._dropdown = QComboBox()
+        self._dropdown.addItems(server_names)
+        self._dropdown.setCurrentIndex(self._dropdown.findText(self._config.get_default_server()))
+        # Connect button
+        self._button = QPushButton('Connect')
+        self._button.clicked.connect(self._new_tab)
+        # Tab view
+        self._tab_view = QTabWidget()
+        self._tab_view.setTabsClosable(True)
+        self._tab_view.tabCloseRequested.connect(self._close_tab)
+        # Layout setup
+        conn_layout = QHBoxLayout()
+        top_layout = QVBoxLayout()
+        conn_layout.addWidget(self._button)
+        conn_layout.addWidget(self._dropdown)
+        conn_widg = QWidget()
+        conn_widg.setLayout(conn_layout)
+        top_layout.addWidget(conn_widg)
+        top_layout.addWidget(self._tab_view)
+        self._widg.setLayout(top_layout)
+
+    @pyqtSlot()
+    def _new_tab(self):
+        server_name = str(self._dropdown.currentText())
+        server = SERVERS[server_name]
+        self._config.set_default_server(server_name)
+        tab = QWidget()  # TODO: Implement
+        self._tab_view.addTab(tab, server_name)
+
+    @pyqtSlot(int)
+    def _close_tab(self, index):
+        self._tab_view.removeTab(index)
+
+    def get_username(self):
+        return self._config.get_username()
