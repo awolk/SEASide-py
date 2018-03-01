61abc65d5ab99d1f75a143a468583c910774fb27
Aidan Wolk <aidanwolk@comcast.net>
2018-03-01 11:29:09 -0800

    Minor cleanup

--- src/app.py
+++ src/app.py
@@ -2,10 +2,10 @@ import sys
 from PyQt5.QtWidgets import QApplication
 from gui.main_window import MainWindow
 
+
 class SEASideApp:
     def __init__(self):
         self._app = QApplication(sys.argv)
-        # self._app.setStyle(QStyleFactory.create('Fusion'))
         self._w = MainWindow()
 
     def run(self):
