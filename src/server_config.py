29a29f185970557b0b72cf8ad1d3824369ff31f7
Sam Smith <Devenvimalpatel@gmail.com>
2018-03-02 12:07:46 -0800

    Bugfix where a number of linux server options pointed to one server.

--- src/server_config.py
+++ src/server_config.py
@@ -1,8 +1,8 @@
 SERVERS = {
     'lnxsrv': 'lnxsrv.seas.ucla.edu',
-    'lnxsrv06': 'lnxsrv07.seas.ucla.edu',
+    'lnxsrv06': 'lnxsrv06.seas.ucla.edu',
     'lnxsrv07': 'lnxsrv07.seas.ucla.edu',
-    'lnxsrv08': 'lnxsrv07.seas.ucla.edu',
+    'lnxsrv08': 'lnxsrv08.seas.ucla.edu',
     'lnxsrv09': 'lnxsrv09.seas.ucla.edu'
 }
 
