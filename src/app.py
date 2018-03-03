2b7f3b2c2685e76f8be842250207f1426a10b18c
Sam Smith <Devenvimalpatel@gmail.com>
2018-03-02 17:34:01 -0800

    Add icon on Windows devices.

--- src/app.py
+++ src/app.py
@@ -1,12 +1,14 @@
 import sys
-from PyQt5.QtWidgets import QApplication
 from gui.main_window import MainWindow
+from PyQt5 import QtGui
+from PyQt5.QtWidgets import *
 
 
 class SEASideApp:
     def __init__(self):
         self._app = QApplication(sys.argv)
         self._w = MainWindow()
+        self._w.setWindowIcon(QtGui.QIcon("../icons/icon.ico"))
 
     def run(self):
         self._w.show()
