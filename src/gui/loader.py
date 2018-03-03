43af63f0b610c8121f0c94e78299b7b70c93180f
Aidan Wolk <aidanwolk@comcast.net>
2018-03-03 12:14:47 -0800

    Prevent hanging on load

--- src/gui/loader.py
+++ src/gui/loader.py
@@ -1,19 +1,62 @@
 from PyQt5.QtWidgets import QLabel
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, QThread, pyqtSignal, QObject, pyqtSlot
+
+
+class LoaderObject(QObject):
+    success = pyqtSignal(str)
+    failure = pyqtSignal(str)
+    finished = pyqtSignal()
+
+    def __init__(self, parent, username, password):
+        super(LoaderObject, self).__init__()
+        self._parent = parent
+        self._username = username
+        self._password = password
+
+    def attempt_connect(self):
+        print('Connecting')
+        if self._username is None:
+            self.failure.emit('')
+            self.finished.emit()
+            return
+        conn = self._parent.get_connection()
+        success = conn.attempt_login(self._username, self._password) if self._password \
+            else conn.attempt_connection(self._username)
+        if success:
+            self.success.emit(self._username)
+        else:
+            self.failure.emit('Login failed')
+        self.finished.emit()
 
 
 class Loader(QLabel):
+    success = pyqtSignal(str)
+    failure = pyqtSignal(str)
+
     def __init__(self, parent):
         self._parent = parent
         super(Loader, self).__init__('Loading...')
         self.setAlignment(Qt.AlignHCenter)
+        self.thread = None
+        self.loader_obj = None
+
+    @pyqtSlot(str)
+    def connect_success(self, username):
+        print('Success, username:', username)
+        self.success.emit(username)
+
+    @pyqtSlot(str)
+    def connect_failure(self, error):
+        print('Failure')
+        self.failure.emit(error)
 
     def connect(self, username=None, password=None):
-        if username is None:
-            return self._parent.connection_failed()
-        conn = self._parent.get_connection()
-        success = conn.attempt_login(username, password) if password else conn.attempt_connection(username)
-        if success:
-            self._parent.connection_successful(username)
-        else:
-            self._parent.connection_failed('Login failed')
+        self.thread = QThread()
+        self.loader_obj = LoaderObject(self._parent, username, password)
+        self.loader_obj.moveToThread(self.thread)
+        self.thread.started.connect(self.loader_obj.attempt_connect)
+        self.loader_obj.finished.connect(self.thread.quit)
+
+        self.loader_obj.success.connect(self.connect_success)
+        self.loader_obj.failure.connect(self.connect_failure)
+        self.thread.start()
--- src/gui/master_tab.py
+++ src/gui/master_tab.py
@@ -1,5 +1,5 @@
 from PyQt5.QtWidgets import QWidget, QVBoxLayout
-from PyQt5.QtCore import QTimer, Qt
+from PyQt5.QtCore import Qt, pyqtSlot
 from gui.loader import Loader
 from gui.login import Login
 from gui.connection_tab import ConnectionTab
@@ -12,17 +12,23 @@ class MasterTab(QWidget):
         self._connection = Connection(server)
         self._config = config
         # Build GUI
-        self._loader = Loader(self)
+        self._loader = self._build_loader()
         self._login = None
         self._layout = QVBoxLayout()
         self._layout.addWidget(self._loader, Qt.AlignCenter)
         self.setLayout(self._layout)
-        # TODO: This should be a separate thread
         self._loader.connect(config.get_username())
 
     def get_connection(self):
         return self._connection
 
+    def _build_loader(self):
+        loader = Loader(self)
+        loader.success.connect(self.connection_successful)
+        loader.failure.connect(self.connection_failed)
+        return loader
+
+    @pyqtSlot(str)
     def connection_failed(self, error=''):
         self._layout.removeWidget(self._loader)  # Remove Loader
         self._loader.deleteLater()
@@ -33,14 +39,15 @@ class MasterTab(QWidget):
     def give_credentials(self, username, password):
         self._layout.removeWidget(self._login)  # Remove Login
         self._login.deleteLater()
-        self._loader = Loader(self)             # Add Loader
+        self._loader = self._build_loader()      # Add Loader
         self._layout.addWidget(self._loader)
-        QTimer.singleShot(0, lambda: self._loader.connect(username, password))
+        self._loader.connect(username, password)
 
+    @pyqtSlot(str)
     def connection_successful(self, username):
         self._layout.removeWidget(self._loader)  # Remove Loader
         self._loader.deleteLater()
-        display = ConnectionTab()          # Add Connection Tab
+        display = ConnectionTab()                # Add Connection Tab
         self._layout.addWidget(display)
         self._config.set_username(username)
         display.start()
